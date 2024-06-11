b8a4511e11b77339009accfaf709ff21876e2f31
karim <450906k@gmail.com>
2024-06-11 17:42:20 +0500

    zip

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { FlyControls } from 'three/addons/controls/FlyControls.js'
+import { BlobReader, TextWriter, ZipReader } from "@zip.js/zip.js"
 
 const AUTH = `Bearer ${localStorage.getItem("token")}`
 const HEADERS = {
@@ -37,35 +38,45 @@ function animate() {
 }
 
 function addMaterial(component, material) {
-    const c = material.color
+    const c = material.Color
     const m = new THREE.MeshBasicMaterial({
         side: THREE.FrontSide,
-        color: new THREE.Color(c.r, c.g, c.b)
+        color: new THREE.Color(c.R, c.G, c.B)
     })
     const geometry = new THREE.BufferGeometry()
 
-    geometry.setFromPoints(component.geometry.points)
-    geometry.setIndex(component.geometry.indices.slice(material.start, material.start + material.count))
-    geometry.computeVertexNormals()
+    geometry.setFromPoints(component.Geometry.Points.map(x => ({ x: x.X, y: x.Y, z: x.Z })))
+    geometry.setIndex(component.Geometry.Indices.slice(material.Start, material.Start + material.Count))
+    //geometry.computeVertexNormals()
 
     const mesh = new THREE.Mesh(geometry, m)
     scene.add(mesh)
 }
 
 function addComponent(component) {
-    for (const material of component.geometry.materials) {
+    for (const material of component.Geometry.Materials) {
         addMaterial(component, material)
     }
 }
 
+async function decompress(blob) {
+    const zipFileReader = new BlobReader(blob)
+    const helloWorldWriter = new TextWriter()
+    const zipReader = new ZipReader(zipFileReader)
+    const firstEntry = (await zipReader.getEntries()).shift()
+    const text = await firstEntry.getData(helloWorldWriter)
+    await zipReader.close()
+    return text
+}
+
 async function addComponentType(model, componentType) {
-    const response = await fetch(`http://185.177.216.241:5047/model/components?modelName=${model}&componentTypeName=${componentType}`, {
+    const response = await fetch(`http://185.177.216.241:5047/model/componentsGeometry/zip?modelName=${model}&componentTypeName=${componentType}`, {
         headers: HEADERS
     })
     if (response.status != 200) {
         return
     }
-    const data = await response.json()
+    const data = JSON.parse(await decompress(await response.blob()))
     for (const component of data) {
         addComponent(component)
     }
@@ -78,6 +89,11 @@ async function addModel(model) {
         headers: HEADERS
     })).json()
     await Promise.all(componentTypes.map(x => addComponentType(model, x)))
+    console.log("FINISH")
 }
 const params = new URLSearchParams(location.search)
-addModel(params.get("model")).then(animate)
+const startTime = performance.now()
+addModel(params.get("model")).then(() => {
+    const endTime = performance.now()
+    console.log("FINISHED. DURATION: " + (endTime - startTime) / 1000 + "s")
+}).then(animate)
