7a8663e1a2e3cb691cf1a50777ff1406f6029f73
karim <450906k@gmail.com>
2024-06-14 21:40:08 +0500

    .

--- render.js
+++ render.js
@@ -1,9 +1,10 @@
-import { Viewer, XKTLoaderPlugin } from "@xeokit/xeokit-sdk"
+import { Viewer, WebIFCLoaderPlugin } from "@xeokit/xeokit-sdk"
 import Stats from 'stats.js'
 import { applyTreeView } from './tree-view'
 import { applyCanvasActions } from './canvas-actions'
+import * as WebIFC from "https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/web-ifc-api.js"
 
-
+console.log("AA")
 const viewer = new Viewer({
     canvasId: "xeokit_canvas",
     transparent: true,
@@ -22,13 +23,21 @@ viewer.scene.on("tick", e => {
 const upload = document.querySelector("input")
 upload.addEventListener("change", async () => {
     const file = await upload.files[0].arrayBuffer()
+    const IfcAPI = new WebIFC.IfcAPI()
 
-    const ifcLoader = new XKTLoaderPlugin(viewer)
+    IfcAPI.SetWasmPath("https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/")
+
+    await IfcAPI.Init()
+
+    const ifcLoader = new WebIFCLoaderPlugin(viewer, {
+        WebIFC,
+        IfcAPI
+    })
 
     const sceneModel = ifcLoader.load({
         id: "myModel",
         edges: true,
-        xkt: file
+        ifc: file
     })
     sceneModel.on("loaded", () => {
         viewer.cameraFlight.jumpTo(sceneModel)
